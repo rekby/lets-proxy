05a78ad0b55e10a86aebfe93b8ee82b30999ded2
Rekby <timofey@koolin.ru>
2016-11-10 11:00:42 +0300

    test http-proxy

--- gitlab/test.sh
+++ gitlab/test.sh
@@ -2,12 +2,33 @@
 
 eval "$(curl -sL https://raw.githubusercontent.com/travis-ci/gimme/master/gimme | GIMME_GO_VERSION=1.7 bash)"
 
-pwd
+mkdir -p $GOPATH/src/github.com/rekby/lets-proxy
+rsync -Hha ./ $GOPATH/src/github.com/rekby/lets-proxy/
 
-go env
-ip addr
+go build gitlab/http-ok.go -o http-ok
+./http-ok &
 
-ping  -nc 5 ya.ru
-ping6 -nc 5 ya.ru
+echo "Test http-ok: "
+curl http://localhost
 
-curl http://ifconfig.io
+DOMAIN="gitlab-test.1gb.ru"
+
+TMP_SUBDOMAIN="tmp-`date +%Y-%m-%d--%H-%M-%S--%N--$RANDOM$RANDOM`.ya"
+TMP_DOMAIN="$TMP_SUBDOMAIN.$DOMAIN"
+
+echo "Tmp domain: $TMP_DOMAIN"
+
+yum install -y wget
+wget https://github.com/rekby/ypdd/releases/download/v0.2/ypdd-linux-amd64.tar.gz
+tar -zxvf ypdd-linux-amd64.tar.gz
+
+
+echo MY IPv6: $MY_IPv6
+./ypdd --sync $DOMAIN add $TMP_SUBDOMAIN AAAA $MY_IPv6
+
+go build github.com/rekby/lets-proxy -o proxy
+
+./proxy &
+
+TEST=`curl http://$TMP_DOMAIN`
+test "$TEST" == "OK"

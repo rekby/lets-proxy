ac02b728b4406689ee7d2a65e75a1eccd3d1b32b
Rekby <timofey@koolin.ru>
2017-01-18 09:59:20 +0300

    small sleep only (default key size).

--- gitlab/test.sh
+++ gitlab/test.sh
@@ -90,7 +90,7 @@ function restart_proxy(){
     if [ -n "$PID" ]; then
         kill -9 "$PID"
     fi
-    ./proxy --test --private-key-len=512 --logout=log.txt --loglevel=debug --real-ip-header=remote-ip,test-remote --additional-headers=https=on,protohttps=on,X-Forwarded-Proto=https --connection-id-header=Connection-ID --cert-json --daemon --pid-file=lets-proxy.pid
+    ./proxy --test --logout=log.txt --loglevel=debug --real-ip-header=remote-ip,test-remote --additional-headers=https=on,protohttps=on,X-Forwarded-Proto=https --connection-id-header=Connection-ID --cert-json --daemon --pid-file=lets-proxy.pid
     sleep 1 # Allow to start, generate keys, etc.
 }
 

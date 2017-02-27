24f023207ee693d341ae34092cdff731928c41e2
Rekby <timofey@koolin.ru>
2017-02-27 14:07:44 +0300

    log requests for built-in proxy

--- http-build-in-proxy.go
+++ http-build-in-proxy.go
@@ -10,6 +10,7 @@ import (
 	"time"
 
 	"github.com/Sirupsen/logrus"
+	"golang.org/x/net/idna"
 )
 
 func acceptConnectionsBuiltinProxy(listeners []*net.TCPListener) {
@@ -22,12 +23,13 @@ func acceptConnectionsBuiltinProxy(listeners []*net.TCPListener) {
 		}
 
 		proxy := &httputil.ReverseProxy{}
+		targetAddrString := tcpAddr.String()
 		proxy.Director = func(req *http.Request) {
 			if req.URL == nil {
 				req.URL = &url.URL{}
 			}
 			req.URL.Scheme = "http"
-			req.URL.Host = tcpAddr.String()
+			req.URL.Host = targetAddrString
 
 			if req.Header == nil {
 				req.Header = make(http.Header)
@@ -35,12 +37,25 @@ func acceptConnectionsBuiltinProxy(listeners []*net.TCPListener) {
 			for _, pair := range additionalHeadersStringPairs {
 				req.Header.Set(pair[0], pair[1])
 			}
-
-			if clientIP, _, err := net.SplitHostPort(req.RemoteAddr); err == nil {
+			clientIP, _, err := net.SplitHostPort(req.RemoteAddr)
+			if err == nil {
 				for _, realIpHeader := range realIPHeaderNamesStrings {
 					req.Header.Set(realIpHeader, clientIP)
 				}
 			}
+
+			if logrus.StandardLogger().Level >= logrus.InfoLevel {
+				asciiDomain, err := idna.ToASCII(req.Host)
+				if err != nil {
+					logrus.Debugf("Can't convert domain to ascii '%v': %v", req.Host, err)
+				}
+				domainPresent := DomainPresent(asciiDomain)
+				logrus.Infof("Start proxy from '%v' to '%v', %v", clientIP, targetAddrString, domainPresent)
+			}
+		}
+
+		proxy.ModifyResponse = func(resp *http.Response) error {
+			return nil
 		}
 
 		tlsListener := tls.NewListener(tcpKeepAliveListener{listener}, createTlsConfig())

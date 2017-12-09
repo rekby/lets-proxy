41082be8c6ce5d62b57304b3667adba12aa98c73
s0t <s0t@users.noreply.github.com>
2017-12-09 18:59:36 +0300

    Update main.go
    Drop 128 bit

--- main.go
+++ main.go
@@ -631,14 +631,18 @@ func createTlsConfig() *tls.Config {
 	tlsConfig := &tls.Config{
 		GetCertificate: certificateGet,
 		CipherSuites: []uint16 {
+/*
 		// 112
-//			tls.TLS_RSA_WITH_3DES_EDE_CBC_SHA, // (0xa) // WEAK
-//			tls.TLS_ECDHE_RSA_WITH_3DES_EDE_CBC_SHA, // (0xc012) // WEAK
+			tls.TLS_RSA_WITH_3DES_EDE_CBC_SHA, // (0xa) // WEAK
+			tls.TLS_ECDHE_RSA_WITH_3DES_EDE_CBC_SHA, // (0xc012) // WEAK
+*/
+/*			
 		// 128
 			tls.TLS_RSA_WITH_AES_128_CBC_SHA, // (0x2f)
 			tls.TLS_RSA_WITH_AES_128_GCM_SHA256, // (0x9c)
 			tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA, // (0xc013)
 			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256, // (0xc02f)
+*/
 		// 256
 			tls.TLS_RSA_WITH_AES_256_CBC_SHA, // (0x35)
 			tls.TLS_RSA_WITH_AES_256_GCM_SHA384, // (0x9d)

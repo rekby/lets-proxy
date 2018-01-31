fe3ccc3da4abd5af836f8f14e5d88affcd4ce560
s0t <s0t@users.noreply.github.com>
2018-01-31 23:12:02 +0300

    Update main.go

--- main.go
+++ main.go
@@ -649,7 +649,7 @@ func createTlsConfig() *tls.Config {
 			tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA, // (0xc014)
 			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384, // (0xc030)
 			tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305, // (0xcca8)
-/			tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305, // (0xcca9)
+			tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305, // (0xcca9)
 		},
 	}
 
